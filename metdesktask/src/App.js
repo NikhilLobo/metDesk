a87831b53de0d5b2f3a7896e313bd0b92deb7b13
Nikhil Lobo <nikhillobo007@gmail.com>
2022-06-01 11:13:11 +0100

    Initial react app setup and added ant design component library

---
+++ metdesktask/src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import { Button } from "antd";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+          <Button type="primary">Button Test</Button>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
